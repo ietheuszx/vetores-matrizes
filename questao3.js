d699e7cd8be3bc85072d8a4133bf1403721a6cf3
ietheuszx <fernandomaranss68@gmail.com>
2024-04-03 16:29:07 -0400

    Questão 3 resolvida

---
+++ questao3.js
@@ -0,0 +1,10 @@
+/*
+Questão 3: Crie um vetor com 10 elementos e remova o último elemento e imprima
+a matriz sem o elemento removido em JavaScript?
+*/ 
+
+let meuArray = [1, 2, 3, 4, 5, 6]
+
+meuArray.pop()
+
+console.log(meuArray)
