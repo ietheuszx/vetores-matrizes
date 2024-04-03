6f25464b482e76797ff396a1d45528ab83b068ec
ietheuszx <fernandomaranss68@gmail.com>
2024-04-03 16:28:16 -0400

    Exemplo de array

---
+++ array.js
@@ -0,0 +1,25 @@
+let meuArray = [];
+
+meuArray.push(10);
+meuArray.push(20);
+meuArray.push(30);
+
+console.log(meuArray);
+
+meuArray.push(40, 50, 60);
+
+console.log(meuArray);
+
+meuArray[2] = 35;
+
+console.log(meuArray);
+
+let elementoRemovido = meuArray.pop();
+
+console.log(elementoRemovido);
+
+console.log(meuArray);
+
+let tamanhoArray = meuArray.length
+
+console.log(tamanhoArray);
