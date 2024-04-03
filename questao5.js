ad1fea640c0154e19858f01bbb0fae51c23c737c
ietheuszx <fernandomaranss68@gmail.com>
2024-04-03 16:29:42 -0400

    Questão 5 resolvida

---
+++ questao5.js
@@ -0,0 +1,12 @@
+/*
+Questão 5: Crie uma matriz 3x3 e imprima o elemento na terceira linha e segunda
+coluna em JavaScript?
+*/
+
+let tresXTres = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+console.log(tresXTres[2][1]);
